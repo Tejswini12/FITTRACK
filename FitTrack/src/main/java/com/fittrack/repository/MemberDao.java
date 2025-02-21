6119c3a28cb43e80c430f54db8d73f156f51ebda
Tejswini12 <101189995+Tejswini12@users.noreply.github.com>
2025-02-22 00:51:07 +0530

    MemberDao.java

--- FitTrack/src/main/java/com/fittrack/repository/MemberDao.java
+++ FitTrack/src/main/java/com/fittrack/repository/MemberDao.java
@@ -8,4 +8,7 @@ import com.fittrack.pojos.Member;
 
 public interface MemberDao extends JpaRepository<Member, Long>{
 
+  @Query("SELECT bp FROM BillPayment bp JOIN bp.member m WHERE m.id = :memberId")
+List<BillPayment> findPaymentsByMemberId(@Param("memberId") Long memberId);
+
 }
